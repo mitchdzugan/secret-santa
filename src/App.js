b7ea7ce852f3c33c8341ac3a16333863bc1a9947
Mitch Dzugan <mitchdzugan@gmail.com>
2020-10-27 07:13:16 -0500

    2020 updates

--- src/App.js
+++ src/App.js
@@ -32,26 +32,36 @@ const makeId = () => {
 class App extends Component {
   constructor (props) {
     super(props);
-    const slsCode = window.localStorage.getItem('sls-code') || makeId();
-    window.localStorage.setItem('sls-code', slsCode);
+      let slsCode = window.localStorage.getItem('sls-code-2020');
+      slsCode = slsCode === "null" ? null : slsCode;
     this.state = {
-      slsCode
-    }
+        slsCode
+    };
 
-    db.collection("Codes").doc(slsCode)
-    .onSnapshot((doc) => {
-        if (!doc.data()) {
-          db.collection("Codes").doc(slsCode).set({
-            slsCode
-          })
-        } else {
-          this.setState({ code: doc.data() })
-        }
-    });
+      this.processCode();
 
     this.unsubYou = () => {};
     this.unsubTo = () => {};
   }
+
+    processCode (code) {
+        const slsCode = code || this.state.slsCode;
+        if (!slsCode) {
+            return;
+        }
+
+        db.collection("Codes").doc(slsCode)
+            .onSnapshot((doc) => {
+                if (!doc.data()) {
+                    db.collection("Codes").doc(slsCode).set({
+                        slsCode
+                    })
+                } else {
+                    this.setState({ code: doc.data() })
+                }
+            });
+
+    }
 
   componentDidUpdate(prevProps, prevState) {
     const currState = this.state;
@@ -76,7 +86,8 @@ class App extends Component {
   }
 
   renderContent() {
-    if (!this.state.code) {
+      window.localStorage.setItem('sls-code-2020', this.state.slsCode);
+    if (this.state.slsCode && !this.state.code) {
       return (
         <div style={{ display: 'flex', textAlign: 'center', flexDirection: 'column', margin: 'auto' }}>
           <img className="loader" width="100px" height="100px" style={{ margin: 'auto' }} src="https://cdn3.iconfinder.com/data/icons/christmas-flatroom/512/santa_claus_old_beard_costume-512.png" />
@@ -85,11 +96,17 @@ class App extends Component {
       );
     }
 
-    if (!this.state.code.personId) {
+    if (!this.state.code || !this.state.code.personId) {
       return (
-        <span>
-          The North Pole Postal Service is running behind, please send <strong>@mitchdzugan</strong> this code: <strong>{this.state.code.slsCode}</strong> to get your computer connected to Santa HQ.
-        </span>
+              <div>
+              <div>
+              The North Pole Postal Service is running behind, please ask <strong>@mitchdzugan</strong> for your connection code and input below to get your computer connected to Santa HQ.
+              </div>
+              <input style={{ marginTop: "10px" }} value={this.state.slsCode || ""} onChange={e => {
+                  this.setState({ slsCode: e.target.value });
+                  this.processCode(e.target.value);
+              }} />
+        </div>
       );
     }
 
@@ -117,6 +134,7 @@ class App extends Component {
 		if (!this.state.you.Wishes || this.state.editWishList) {
 			return ( 
         <div style={{ display: 'flex', flexDirection: 'column' }} >
+
           <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }} >
             <div>
               <input 
@@ -138,6 +156,9 @@ class App extends Component {
                                          />}
           {!this.state.isPreview ? null : <div dangerouslySetInnerHTML={{ __html: markdown.toHTML(this.state.youWishes || this.state.you.Wishes || '') }} />}
           <div style={{ textAlign: 'center', marginTop: '20px' }} > <button onClick={submit} >Submit</button> </div> 
+              <div style={{ marginTop: "10px" }}>
+              You are the Santa for <strong>{this.state.to.Name}</strong>, you will be able to see their wish list and anonymously chat with them once you both have submitted yours.
+          </div>
         </div>
       );
 		}
@@ -156,9 +177,6 @@ class App extends Component {
             <span style={{ fontWeight: 'bold' }} >
               {Name}&nbsp;
             </span>
-            <span style={{ fontSize: '9px' }} >
-              (aka {RName})
-            </span>
           </p>
           <p>
             We are still waiting to receive their letter to you. It will show up here as soon as its available.
@@ -172,8 +190,8 @@ class App extends Component {
       <div>
         <p>Dear Santa <strong>{this.state.you.Name}</strong>,</p>
         <p style={{ marginLeft: '20px' }}>
-          My name is <strong>{Name}</strong>, this year I tried to wfh as little as possible and only raged at 
-          leads when I was really triggered so I think I deserve to be on the nice list. If you
+            My name is <strong>{Name}</strong>, this year I wore a mask whenever I was in public, resisted the urge to break quarantine to get kbbq,
+        and only took advantage of wfh when I was feeling extremely lazy so I think I deserve to be on the nice list. If you
           agree please get me something from this list:
         </p>
         <p style={{ marginLeft: '20px' }}
@@ -181,7 +199,7 @@ class App extends Component {
         />
         <p style={{ marginLeft: '20px' }}>My chimney is currently out of order so you can just send my gift to <strong>{Address}</strong></p>
         <p style={{ marginTop: '40px' }}>-Merry Christmas,</p>
-        <p style={{ marginTop: '-5px', marginLeft: '5px' }}>{Name} <span style={{ fontSize: '9px' }} >(aka {RName})</span></p>
+        <p style={{ marginTop: '-5px', marginLeft: '5px' }}> {Name} </p>
       </div>
     );
   }
